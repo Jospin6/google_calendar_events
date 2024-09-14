639302c3f6844337a683fb30a0ca40c964b045a3
jospin6 <jospinndagano1@gmail.com>
2024-09-14 16:15:04 +0200

    Install active text, create event form and add some helpers to the application helpers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,48 +1,58 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy, :sync_event_with_google]
+  before_action :set_event, only: %i[ show edit update destroy ]
   before_action :authenticate_user!
 
+  # GET /events or /events.json
   def index
     @events = current_user.events
   end
 
-  def show; end
+  # GET /events/1 or /events/1.json
+  def show
+  end
 
+  # GET /events/new
   def new
     @event = Event.new
   end
 
-  def edit; end
+  # GET /events/1/edit
+  def edit
+  end
 
+  # POST /events or /events.json
   def create
     @event = Event.new(event_params)
+
     respond_to do |format|
       if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.html { redirect_to event_url(@event), notice: "Event was successfully created." }
         format.json { render :show, status: :created, location: @event }
       else
-        format.html { render :new }
+        format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @event.errors, status: :unprocessable_entity }
       end
     end
   end
 
+  # PATCH/PUT /events/1 or /events/1.json
   def update
     respond_to do |format|
       if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+        format.html { redirect_to event_url(@event), notice: "Event was successfully updated." }
         format.json { render :show, status: :ok, location: @event }
       else
-        format.html { render :edit }
+        format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @event.errors, status: :unprocessable_entity }
       end
     end
   end
 
+  # DELETE /events/1 or /events/1.json
   def destroy
     @event.destroy
     respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+      format.html { redirect_to events_url, notice: "Event was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -54,8 +64,8 @@ class EventsController < ApplicationController
     Event.all.each do |event|
       @events << {
         title: event.title,
-        start: event.start_date.strftime("%Y-%m-%d"), 
-        end: event.end_date.strftime("%Y-%m-%d"),
+        end: event.start_date.strftime("%Y-%m-%d"), 
+        start: event.end_date.strftime("%Y-%m-%d"),
         url: event_path(event)
       }
     end
@@ -91,7 +101,6 @@ class EventsController < ApplicationController
   end
 
   private
-
     def set_event
       @event = Event.find(params[:id])
     end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,33 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_rich_text :description
+  after_create :publish_event_to_gcal
+  after_update :update_event_on_gcal
+  before_destroy :remove_event_from_gcal
+  
+  def email_guest_list
+    return if self.guest_list.nil?
+    
+    guest_list.split(", ")
+  end
+
+  def validate_event_dates
+    return if start_date.nil? || end_date.nil?
+    
+    if start_date > end_date
+      errors.add(:start_date, 'must be less than end date')
+    end
+  end
+
+  def publish_event_to_gcal
+    self.create_google_event(self)
+  end
+
+  def update_event_on_gcal
+    self.edit_google_event(self)
+  end
+
+  def remove_event_from_gcal
+    self.delete_google_event(self)
+  end
 end
