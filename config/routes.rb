c24e26ac4cab88242e8add4a706351db50cbebdd
jospin6 <jospinndagano1@gmail.com>
2024-09-15 01:52:28 +0200

    Add display fle to the delete, destroy, and edit event links

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :events
+  resources :events do 
+    patch :sync_event_with_google, on: :member
+  end
   devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
     omniauth_callbacks: "users/omniauth_callbacks"
   }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "home#index"
 end
